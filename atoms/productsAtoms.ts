0cf4d1687f68ea5f961684fcbf6c438ac6e4a176
ahmed nasser <ahmednas197@gmail.com>
2023-05-14 23:40:25 +0300

    added basic jotai exmaple

---
+++ atoms/productsAtoms.ts
@@ -0,0 +1,34 @@
+import { atom } from "jotai";
+import { DUMMY_PRODUCTS, ProductI } from "../dummy-products";
+
+export const productsAtoms = atom<ProductI[]>(DUMMY_PRODUCTS);
+export const searchAtom = atom("");
+export const resetSearchAtom = atom(null, (_get, set) => {
+  set(searchAtom, "");
+});
+export const filteredProductsAtom = atom((get) => {
+  const search = get(searchAtom);
+  const products = get(productsAtoms);
+  if (!search) return products;
+  return products.filter((product) => product.title.includes(search));
+});
+
+export const informationBarAtom = atom((get) => {
+  const totalProductsNumber = get(productsAtoms).length;
+  const filteredProductsNumber = get(filteredProductsAtom).length;
+  return {
+    totalProductsNumber,
+    filteredProductsNumber,
+  };
+});
+
+export const sortAtom = atom("asc");
+
+export const sortedProductsAtom = atom((get) => {
+  const sort = get(sortAtom);
+  const products = get(filteredProductsAtom);
+  if (sort === "asc") {
+    return [...products].sort((a, b) => a.price - b.price);
+  }
+  return [...products].sort((a, b) => b.price - a.price);
+});
